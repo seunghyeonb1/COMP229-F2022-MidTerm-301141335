db4375bf6a3e5e7af6f44afeee3b2f7b5ab40d4a
Seunghyeon Ban <munjh564@gmail.com>
2022-10-29 18:59:14 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb+srv://seunghyeonban:8jp4SpcVm7MmN3FS@cluster0.jbkrpc1.mongodb.net/book_list?retryWrites=true&w=majority"
+
+ 
+};
